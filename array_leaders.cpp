5822eb9133b2c3bd57788a81b1e42b090f9fb337
Eswar sai Yarra <119247902+eswarsaiyarra2602@users.noreply.github.com>
2024-06-18 18:24:36 +0530

    Create array_leaders.cpp

---
+++ array_leaders.cpp
@@ -0,0 +1,19 @@
+//gfg - array leaders
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(int n, int arr[]) {
+        // Code here
+        vector<int>result;
+        int maxElement = arr[n-1];
+        for( int i = n-1 ; i>=0 ; i--){
+            if( arr[i] >= maxElement ){
+                result.push_back(arr[i]);
+            }
+            maxElement = max(maxElement , arr[i]); 
+        }
+        reverse(result.begin() , result.end());
+        return result;
+    }
+};

fb41cd1a3e47bf7282a76a9380e9f522daafdb13
Eswar sai Yarra <119247902+eswarsaiyarra2602@users.noreply.github.com>
2024-06-25 10:12:44 +0530

    Create binary_search1.cpp

---
+++ ARRAY/binary_search1.cpp
@@ -0,0 +1,21 @@
+//leetcode-153 , find min in rotated sorted array 
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0 , right = nums.size()-1;
+        int minElement = INT_MAX;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(nums[left]<=nums[mid]){
+                minElement = min(minElement, nums[left]);
+                left = mid+1;
+            }
+            else if(nums[mid]<=nums[right]){
+                minElement = min(minElement , nums[mid]) ;
+                right = mid-1;
+            }
+        }
+        return minElement ; 
+    }
+};

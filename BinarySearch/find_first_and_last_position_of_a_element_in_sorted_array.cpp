2a46a8117f56ed5ef9c5471afe6e2e1d65f5b416
Eswar sai Yarra <119247902+eswarsaiyarra2602@users.noreply.github.com>
2024-06-25 10:23:30 +0530

    Create find_first_and_last_position_of_a_element_in_sorted_array.cpp

---
+++ BinarySearch/find_first_and_last_position_of_a_element_in_sorted_array.cpp
@@ -0,0 +1,47 @@
+//leetcode-34 
+
+class Solution {
+public:
+    int getLeftMost(vector<int>&nums, int target){
+        int left_most = -1;
+        int left = 0 , right = nums.size()-1;
+        while(left <= right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target){
+                left_most = mid ;
+                right = mid-1;
+            }
+            else if(nums[mid]>target){
+                right = mid-1;
+            }
+            else{
+                left= mid+1;
+            }
+        }
+        return left_most;
+    }
+    int getRightMost(vector<int>&nums, int target){
+        int right_most = -1;
+        int left = 0 , right = nums.size()-1;
+        while(left <= right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target){
+                right_most = mid;
+                left = mid+1;
+            }
+            else if(nums[mid]>target){
+                right = mid-1;
+            }
+            else{
+                left= mid+1;
+            }
+        }
+        return right_most;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int left_most = getLeftMost(nums , target);
+        if(left_most ==-1){return {-1,-1};}
+        int right_most = getRightMost(nums , target);
+        return  {left_most, right_most};
+    }
+};
